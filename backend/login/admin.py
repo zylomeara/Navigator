afaff857dd6fbe5571e0c8a6652aa80d7a925e57
zylomeara <baback.artem@gmail.com>
2019-05-19 22:44:15 +0300

    Регистрация моделей

--- backend/login/admin.py
+++ backend/login/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Manager, Admin, Courier
 # Register your models here.
+
+admin.site.register(Manager)
+admin.site.register(Admin)
+admin.site.register(Courier)
--- backend/orderapi/admin.py
+++ backend/orderapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client, Order, Transportation, Task
 
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Transportation)
+admin.site.register(Task)
