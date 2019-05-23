7c40681ca0009ab9165561ac9679f60acc1fdd12
zylomeara <baback.artem@gmail.com>
2019-05-23 16:44:58 +0300

    Настроил Задачи и Перевозки для курьеров

--- frontend/src/Workplace/Courier/index.tsx
+++ frontend/src/Workplace/Courier/index.tsx
@@ -1,27 +1,52 @@
 import * as React from 'react';
-import { Breadcrumb, Layout, Menu } from "antd";
+import { Breadcrumb, Icon, Layout, Menu, Tabs } from "antd";
+import ClientManager from "../../ClientManager";
+import OrderManager from "../../OrderManager";
+import TransportationManager from "../../TransportationManager";
+import TaskManager from "../../TaskManager";
+import { useState } from "react";
 
 const {
   Header,
   Content,
-  Footer
+  Footer,
+  Sider
 } = Layout;
 
+type Tabs = 'transportations' | 'tasks'
+
 const Courier = (props: any) => {
+  let [tab, setTab] = useState<Tabs>('tasks');
 
   return (
-    <>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Hello courier</div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </>
-  )
+<Layout style={{height: '100%'}}>
+      <Sider width={200} style={{ background: '#fff' }}>
+        <Menu
+          mode="inline"
+          onSelect={(obj) => {setTab((obj.key as Tabs))}}
+          selectedKeys={[tab]}
+          style={{ height: '100%' }}
+        >
+          <Menu.Item key="tasks"><Icon type={'solution'}/>Задачи</Menu.Item>
+          <Menu.Item key="transportations"><Icon type={'car'}/>Перевозки</Menu.Item>
+        </Menu>
+      </Sider>
+
+      {/*<Content style={{ padding: '0 50px' }}>*/}
+      {/*  <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Hello manager</div>*/}
+      {/*</Content>*/}
+      <Tabs activeKey={tab} renderTabBar={() => <></>}>
+        <Tabs.TabPane key={'transportations'}>
+          <TransportationManager />
+          {/*1*/}
+        </Tabs.TabPane>
+        <Tabs.TabPane key={'tasks'}>
+          <TaskManager />
+          {/*1*/}
+        </Tabs.TabPane>
+      </Tabs>
+
+    </Layout>  )
 };
 
 export default Courier;
