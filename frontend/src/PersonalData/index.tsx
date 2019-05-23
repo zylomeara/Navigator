b8944b59cb4c45cfd28b58940d4a1e25c8213555
zylomeara <baback.artem@gmail.com>
2019-05-24 00:39:21 +0300

    Выравнивание личной информации по центру

--- frontend/src/PersonalData/index.tsx
+++ frontend/src/PersonalData/index.tsx
@@ -12,52 +12,54 @@ const PersonalData = (props: any) => {
 
   return (
     <div className={'PersonalData'}>
-      <Row gutter={8}>
+      <div>
+        <Row gutter={8}>
         <Col span={12} className={'title'}>Логин:</Col>
         <Col span={12} className={'description'}>{profile.username}</Col>
       </Row>
-      <Row gutter={8}>
-        <Col span={12} className={'title'}>Имя:</Col>
-        <Col span={12} className={'description'}>{profile.first_name}</Col>
-      </Row>
-      <Row gutter={8}>
-        <Col span={12} className={'title'}>Фамилия:</Col>
-        <Col span={12} className={'description'}>{profile.last_name}</Col>
-      </Row>
-      <Row gutter={8}>
-        <Col span={12} className={'title'}>Дата регистрации:</Col>
-        <Col span={12} className={'description'}>{profile.date_joined}</Col>
-      </Row>
-      <Row gutter={8}>
-        <Col span={12} className={'title'}>Дата последнего входа:</Col>
-        <Col span={12} className={'description'}>{profile.last_login}</Col>
-      </Row>
-      <Row gutter={8}>
-        <Col span={12} className={'title'}>Номер телефона:</Col>
-        <Col span={12} className={'description'}>{profile.phone_number}</Col>
-      </Row>
-      {profile.transport && <Row gutter={8}>
-        <Col span={12} className={'title'}>Транспорт:</Col>
-        <Col span={12} className={'description'}>{profile.transport}</Col>
-      </Row>}
-      {profile.work_place && <Row gutter={8}>
-        <Col span={12} className={'title'}>Рабочее место:</Col>
-        <Col span={12} className={'description'}>{profile.work_place}</Col>
-      </Row>}
-      <Row gutter={8}>
-        <Col span={12} className={'title'}>Должность:</Col>
-        <Col span={12} className={'description'}>{
-          data.position === 'courier'
-            ? 'Курьер'
-            : data.position === 'manager'
-            ? 'Менеджер'
-            : 'Админ'
-        }</Col>
-      </Row>
-      {/*<Row gutter={8}>*/}
-      {/*  <Col span={12} className={'title'}></Col>*/}
-      {/*  <Col span={12} className={'description'}></Col>*/}
-      {/*</Row>*/}
+        <Row gutter={8}>
+          <Col span={12} className={'title'}>Имя:</Col>
+          <Col span={12} className={'description'}>{profile.first_name}</Col>
+        </Row>
+        <Row gutter={8}>
+          <Col span={12} className={'title'}>Фамилия:</Col>
+          <Col span={12} className={'description'}>{profile.last_name}</Col>
+        </Row>
+        <Row gutter={8}>
+          <Col span={12} className={'title'}>Дата регистрации:</Col>
+          <Col span={12} className={'description'}>{profile.date_joined}</Col>
+        </Row>
+        <Row gutter={8}>
+          <Col span={12} className={'title'}>Дата последнего входа:</Col>
+          <Col span={12} className={'description'}>{profile.last_login}</Col>
+        </Row>
+        <Row gutter={8}>
+          <Col span={12} className={'title'}>Номер телефона:</Col>
+          <Col span={12} className={'description'}>{profile.phone_number}</Col>
+        </Row>
+        {profile.transport && <Row gutter={8}>
+          <Col span={12} className={'title'}>Транспорт:</Col>
+          <Col span={12} className={'description'}>{profile.transport}</Col>
+        </Row>}
+        {profile.work_place && <Row gutter={8}>
+          <Col span={12} className={'title'}>Рабочее место:</Col>
+          <Col span={12} className={'description'}>{profile.work_place}</Col>
+        </Row>}
+        <Row gutter={8}>
+          <Col span={12} className={'title'}>Должность:</Col>
+          <Col span={12} className={'description'}>{
+            data.position === 'courier'
+              ? 'Курьер'
+              : data.position === 'manager'
+              ? 'Менеджер'
+              : 'Админ'
+          }</Col>
+        </Row>
+        {/*<Row gutter={8}>*/}
+        {/*  <Col span={12} className={'title'}></Col>*/}
+        {/*  <Col span={12} className={'description'}></Col>*/}
+        {/*</Row>*/}
+      </div>
     </div>
   )
 };
